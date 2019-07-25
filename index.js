813c045f1555b1393a4906c3b9bf4deb9778d95b
Sonav <sonavagarwal@gmail.com>
2019-07-24 17:59:39 -0700

    wut

--- index.js
+++ index.js
@@ -2,6 +2,12 @@
 
 var conForm = document.getElementById("contactForm");
 
+var confirm = document.createElement("p");
+var node = document.createTextNode("Your message has been recorded.");
+
+confirm.appendChild(node);
+
+var parent = document.getElementById()
 
 function fadeout() {
     conForm.style.display="none";

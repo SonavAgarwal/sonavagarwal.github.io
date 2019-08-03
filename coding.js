2397d9ad29cd18be59783e31e7162de3ac8ebc03
Sonav <sonavagarwal@gmail.com>
2019-08-02 19:33:39 -0700

    zoomable home, sleeping, and coding update

--- coding.js
+++ coding.js
@@ -1,4 +1,4 @@
-var toptext = "";
+var toptext = "#Some fake Python code<br>";
 
 var chars = [' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
@@ -9,6 +9,10 @@ var c = 0;
 var times = 0;
 
 var wrd = "";
+
+var choice = 0;
+
+var prevtabs = 0;
 
 var funcs = [];
 
@@ -21,9 +25,26 @@ function add() {
     for(var j = 0; j < Math.floor((Math.random() * 20) + 5); j++) {
         wrd += chars[Math.floor(Math.random() * 27)];
     }
-    funcs = [`for x in range(${a}):<br>      `, `x = ${c}<br>while(x < ${a}):<br>      x += ${b}<br>      `, `print('${wrd}')<br>`, `${wrd} = ${a}<br>`, `#${wrd}<br>`];
+    funcs = [`for x in range(${a}):<br>`, `while(x < ${a}):<br>      x += ${b}<br>`, `print('${wrd}')<br>`, `${wrd} = ${a}<br>`, `#${wrd}<br>`, `${wrd} = input("${wrd}")<br>`, `${wrd} = [${a}, ${b}, ${c}]<br>`, `<br>`];
     
-    toptext += funcs[Math.floor(Math.random() * 5)]
+    choice = Math.floor(Math.random() * 8);
+
+    for (var k = 0; k < prevtabs; k++) {
+        toptext += "      ";
+    }
+
+    toptext += funcs[choice];
+
+    if (choice < 2) {
+        prevtabs += 1;
+        
+    } else {
+        // console.log(funcs[choice]);
+        prevtabs = 0;
+    }
+    // console.log(choice);
+    // console.log(prevtabs);
+
     document.getElementById("topara").innerHTML = toptext;
     if (times >= 20) {
         clearInterval(inty);
